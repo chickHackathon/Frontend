4069e02c0b99e790b6b995ae01cba805dc8ce48f
chloe_choi <plumxeun@gmail.com>
2024-08-22 12:43:34 +0900

    [FEAT] quill image-uploader add

--- src/pages/MemoPage.tsx
+++ src/pages/MemoPage.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
+import ImageUploader from 'quill-image-uploader';
+import 'quill-image-uploader/dist/quill.imageUploader.min.css';
+
+Quill.register('modules/imageUploader', ImageUploader);
 
 const MemoPage: React.FC = () => {
     const [memoContent, setMemoContent] = useState<string>('');
@@ -11,6 +15,26 @@ const MemoPage: React.FC = () => {
         if (quillRef.current && !quillInstanceRef.current) {
             quillInstanceRef.current = new Quill(quillRef.current, {
                 theme: 'snow',
+                modules: {
+                    toolbar: [
+                        [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+                        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                        ['bold', 'italic', 'underline'],
+                        ['image']
+                    ],
+                    imageUploader: {
+                        upload: (file: File) => {
+                            return new Promise((resolve, reject) => {
+                                const reader = new FileReader();
+                                reader.onload = () => {
+                                    resolve(reader.result as string);
+                                };
+                                reader.onerror = reject;
+                                reader.readAsDataURL(file);
+                            });
+                        }
+                    }
+                }
             });
 
             quillInstanceRef.current.on('text-change', () => {
