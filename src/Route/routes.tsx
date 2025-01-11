6e7cd513d8c6b1bf42356630bbd24e08d793eda1
daeun0726 <delee2008@naver.com>
2025-01-11 21:17:00 +0900

    route 수정

--- src/Route/routes.tsx
+++ src/Route/routes.tsx
@@ -5,9 +5,10 @@ import LoginPage from '../pages/LoginPage';
 import MemoPage from '../pages/MemoPage';
 import Register from '../pages/Register/Register';
 import Login from '../pages/Login/Login';
-import StudyDetail from "../pages/studyDetail/StudyDetail";
-import StudyRecord from "../pages/studyRecord/StudyRecord";
-
+import StudyDetail from '../pages/studyDetail/StudyDetail';
+import StudyRecord from '../pages/studyRecord/StudyRecord';
+import StudyList from '../pages/StudyList';
+import StudyCreate from '../pages/studyCreate/StudyCreate';
 
 const AppRoutes: React.FC = () => {
   const handleLogin = () => {
@@ -21,8 +22,10 @@ const AppRoutes: React.FC = () => {
       <Route path="/memo" element={<MemoPage />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-        <Route path="/study-detail/:id" element={<StudyDetail />} />
-        <Route path="/study-record/:id" element={<StudyRecord />} />
+      <Route path="/studylist" element={<StudyList />} />
+      <Route path="/study-detail/:id" element={<StudyDetail />} />
+      <Route path="/study-record/:id" element={<StudyRecord />} />
+      <Route path="/studycreate" element={<StudyCreate />} />
     </Routes>
   );
 };
---
+++ src/pages/studyCreate/StudyCreate.tsx
@@ -0,0 +1,7 @@
+export default function StudyCreate() {
+  return (
+    <div>
+      <h1>StudyCreate</h1>
+    </div>
+  );
+}
