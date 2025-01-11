46a19803dfe3d7259a9f67db7e675c900c9ebaa1
cgw-notebook <mangang0713@gmail.com>
2025-01-11 20:59:27 +0900

    refactor: 위치 변경

--- src/pages/studyRecord/StudyRecord.tsx
+++ src/pages/studyRecord/StudyRecord.tsx
@@ -1,15 +1,15 @@
-import MapComponent from "../../components/StudyRecord/MapComponent";
-import TopBar from "../../components/Register/TopBar";
-import StoreListComponent from "../../components/StudyRecord/StoreListComponent";
-import {useState} from "react";
+import MapComponent from '../../components/StudyRecord/MapComponent';
+import StoreListComponent from '../../components/StudyRecord/StoreListComponent';
+import { useState } from 'react';
+import TopBar from '../../shared/ui/TopBar';
 
 export default function StudyRecord() {
-    const [cafes, setCafes] = useState<any[]>([]);
-    return (
-        <div>
-            <TopBar>스터디 내역</TopBar>
-            <MapComponent  setCafes={setCafes}/>
-            <StoreListComponent  cafes={cafes}/>
-        </div>
-    );
+  const [cafes, setCafes] = useState<any[]>([]);
+  return (
+    <div>
+      <TopBar>스터디 내역</TopBar>
+      <MapComponent setCafes={setCafes} />
+      <StoreListComponent cafes={cafes} />
+    </div>
+  );
 }
