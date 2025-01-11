0b795542a338bde53dab93f62916b1c195bde154
daeun0726 <delee2008@naver.com>
2025-01-12 02:18:33 +0900

    feat: studycreate 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './Route/routes';
-import SplashScreen from './components/SplashScreen';
+// import AppRoutes from './Route/routes';
+// import SplashScreen from './components/SplashScreen';
+import StudyCreate from '../src/pages/studyCreate/StudyCreate';
 
 const App: React.FC = () => {
-  const [showSplash, setShowSplash] = useState(true);
+  // const [showSplash, setShowSplash] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 3000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShowSplash(false);
+  //   }, 3000);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
-    <Router>
-      {showSplash ? (
-        <SplashScreen />
-      ) : (
-        <div>
-          <AppRoutes />
-        </div>
-      )}
-    </Router>
+    // <Router>
+    //   {showSplash ? (
+    //     <SplashScreen />
+    //   ) : (
+    //     <div>
+    //       <AppRoutes />
+    //     </div>
+    //   )}
+    // </Router>
+    <StudyCreate />
   );
 };
 
