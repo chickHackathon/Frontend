49a02b4cdb8181430a6435acf17bc81a87eacbfc
cgw-notebook <mangang0713@gmail.com>
2025-01-12 08:32:36 +0900

    ㅇㅇㅇ

--- src/components/Register/RegisterForm.tsx
+++ src/components/Register/RegisterForm.tsx
@@ -49,16 +49,18 @@ const RegisterForm = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    const formData = {
+      id: idValue,
+      password: passwordValue,
+      nickname: nicknameValue,
+      email: emailValue,
+      location: locationValue,
+      latitude: latitudeValue,
+      longitude: longitudeValue,
+    };
+
     try {
-      const response = await axiosInstance.post('/member/signup', {
-        id: idValue,
-        password: passwordValue,
-        nickname: nicknameValue,
-        email: emailValue,
-        location: locationValue,
-        latitude: latitudeValue,
-        longitude: longitudeValue,
-      });
+      const response = await axiosInstance.post('/member/signup', formData);
       console.log('회원가입 성공:', response.data);
       navigate('/login');
     } catch (error: any) {
