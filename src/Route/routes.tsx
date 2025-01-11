e9873d763a042171e53b3170c6e34b85a797a935
chloe_choi <plumxeun@gmail.com>
2025-01-11 21:22:30 +0900

    temp router added

--- src/Route/routes.tsx
+++ src/Route/routes.tsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Camera from '../pages/Camera';
-import LoginPage from '../pages/LoginPage';
 import MemoPage from '../pages/MemoPage';
 import Register from '../pages/Register/Register';
-import Login from '../pages/Login/Login';
-import StudyDetail from '../pages/studyDetail/StudyDetail';
-import StudyRecord from '../pages/studyRecord/StudyRecord';
-import StudyList from '../pages/StudyList';
-import StudyCreate from '../pages/studyCreate/StudyCreate';
+import StudyDetail from "../pages/studyDetail/StudyDetail";
+import StudyRecord from "../pages/studyRecord/StudyRecord";
+import TempPage from "../pages/TempPage";
+import StudyList from "../pages/StudyList";
+import StudyCreate from "../pages/studyCreate/StudyCreate";
+import Login from "../pages/Login/Login";
 
 const AppRoutes: React.FC = () => {
-  const handleLogin = () => {
-    console.log('LoginPage set');
-  };
 
   return (
     <Routes>
       <Route path="/camera" element={<Camera />} />
-      <Route path="/" element={<LoginPage onComplete={handleLogin} />} />
+      <Route path="/" element={<TempPage/>} />
       <Route path="/memo" element={<MemoPage />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/studylist" element={<StudyList />} />
-      <Route path="/study-detail/:id" element={<StudyDetail />} />
-      <Route path="/study-record/:id" element={<StudyRecord />} />
-      <Route path="/studycreate" element={<StudyCreate />} />
+        <Route path="/studycreate" element={<StudyCreate />} />
+        <Route path="/studylist" element={<StudyList />} />
+        <Route path="/study-detail/:id" element={<StudyDetail />} />
+        <Route path="/study-record/:id" element={<StudyRecord />} />
     </Routes>
   );
 };
