9a580d425c9d54a38c73ceb74a64290f94c33fc9
chloe_choi <plumxeun@gmail.com>
2025-01-11 21:19:02 +0900

    temp router

--- src/Route/routes.tsx
+++ src/Route/routes.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Camera from '../pages/Camera';
-import LoginPage from '../pages/LoginPage';
-import MemoPage from '../pages/MemoPage';
 import Register from '../pages/Register/Register';
 import Login from '../pages/Login/Login';
 import StudyDetail from "../pages/studyDetail/StudyDetail";
 import StudyRecord from "../pages/studyRecord/StudyRecord";
+import TempPage from "../pages/TempPage";
 
 
 const AppRoutes: React.FC = () => {
-  const handleLogin = () => {
-    console.log('LoginPage set');
-  };
 
   return (
     <Routes>
-      <Route path="/camera" element={<Camera />} />
-      <Route path="/" element={<LoginPage onComplete={handleLogin} />} />
-      <Route path="/memo" element={<MemoPage />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<TempPage />} />
+      <Route path="/signup" element={<Register />} />
       <Route path="/login" element={<Login />} />
-        <Route path="/study-detail/:id" element={<StudyDetail />} />
-        <Route path="/study-record/:id" element={<StudyRecord />} />
+        <Route path="/study-detail" element={<StudyDetail />} />
+        <Route path="/study-record" element={<StudyRecord />} />
     </Routes>
   );
 };
