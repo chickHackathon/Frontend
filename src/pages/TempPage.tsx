56641cfc0808cbff1fc345d02d6b4c815e0a8685
chloe_choi <plumxeun@gmail.com>
2025-01-11 21:27:10 +0900

    temp router added

--- src/pages/TempPage.tsx
+++ src/pages/TempPage.tsx
@@ -12,6 +12,8 @@ const TempPage: React.FC = () => {
             <Title>임시 라우터 접근 페이지</Title>
             <button onClick={() => navigate('/login')}>로그인 페이지 접근</button>
             <button onClick={() => navigate('/signup')}>회원가입 페이지 접근</button>
+            <button onClick={() => navigate('/studycreate')}>스터디 생성 페이지 접근</button>
+            <button onClick={() => navigate('/studylist')}>스터디 리스트 페이지 접근</button>
             <button onClick={() => navigate('/study-detail')}>스터디 디테일 페이지 접근</button>
             <button onClick={() => navigate('/study-record')}>스터디 레코드 페이지 접근</button>
         </OnboardingContainer>
